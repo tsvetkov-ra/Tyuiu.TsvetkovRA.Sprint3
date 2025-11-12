713060e435c302a191b56f82911d5ab047f93e24
Admin <artykleshine@gmail.com>
2025-11-12 19:00:44 +0300

    Task7

---
+++ Tyuiu.TsvetkovRA.Sprint3.Task7.V26.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TsvetkovRA.Sprint3.Task7.V26.Lib
+{
+    public class DataService : ISprint3Task7V26
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                
+                double step1 = 2 * x - 0.5;
+                double step2 = 1 + Math.Sin(x);
+                double step3 = step2 / step1;
+                double step4 = 5 - x * 3 + step3;
+                y = step4;
+                if (x == 0.25)
+                {
+                    y = 0;
+                }
+
+                valueArray[count] = Math.Round(y, 2);
+                count++;
+                
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint3.Task7.V26/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.TsvetkovRA.Sprint3.Task7.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       DataService ds = new DataService();
+        int startValue = -5;
+
+        int stopValue = 5;
+
+        int len = ds.GetMassFunction(startValue,stopValue).Length;
+
+        double[] valueArray;
+
+        valueArray = new double[len];
+
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил : Цветков Роман Андреевич | ИИПб-25-1                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Напишите программу, которая которая выводит таблицу значения функции    *");
+        Console.WriteLine("* на диапазоне [-5, 5]                                                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("+-----------+------------+");
+        Console.WriteLine("|    X      |   f(x)     |");
+        Console.WriteLine("+-----------+------------+");
+
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}      |   {1, 5:f2}    |"      , startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+-----------+------------+");
+        Console.ReadKey();
+    }
+}
